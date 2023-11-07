f081955b7b24ac60b641a6bac546ef378e199da3
Troy Martin <beef.erikson.studios@gmail.com>
2023-11-07 16:32:30 -0500

    Asteroid spawning implemented.

--- game/version1/asteroid.py
+++ game/version1/asteroid.py
@@ -1,9 +1,10 @@
-from game import resources
+from game import resources, load
 import pyglet
 
 game_window = pyglet.window.Window(800, 600)
 
 player_ship = pyglet.sprite.Sprite(img=resources.player_image, x=400, y=300)
+asteroids = load.asteroids(3, player_ship.position)
 
 score_label = pyglet.text.Label(text='Score: 0', x=10, y=game_window.height - 20)
 level_label = pyglet.text.Label(text='My Amazing Game', x=game_window.width//2,
@@ -20,6 +21,10 @@ def on_draw():
 
     # Draws player
     player_ship.draw()
+
+    # Draws asteroids
+    for asteroid in asteroids:
+        asteroid.draw()
     
 
 if __name__ == '__main__':
--- game/version1/game/load.py
+++ game/version1/game/load.py
@@ -4,19 +4,28 @@ import math
 from . import resources
 
 
-def asteroids(num_asteroids: int) -> list:
+def asteroids(num_asteroids: int, player_position: tuple) -> list:
+    """ Creates asteroids away from the player and returns a list of their positions. """
     asteroid_list = []
+
     for x in range(num_asteroids):
-        asteroid_x = random.randint(0, 800)
-        asteroid_y = random.randint(0, 600)
-        new_asteroid = pyglet.sprite.Sprite(img=resources.asteroid_image,
-                                            x=asteroid_x, y=asteroid_y)
+        asteroid_x, asteroid_y, _ = player_position
+
+        # If distance is within 100 pixels, respawn random position
+        while distance((asteroid_x, asteroid_y), player_position) < 100:
+            asteroid_x = random.randint(0, 800)
+            asteroid_y = random.randint(0, 600)
+
+        # Creates asteroids and sets a random rotation
+        new_asteroid = pyglet.sprite.Sprite(
+            img=resources.asteroid_image, x=asteroid_x, y=asteroid_y)
         new_asteroid.rotation = random.randint(0, 360)
+
         asteroid_list.append(new_asteroid)
 
     return asteroid_list
 
 
-def distance(point_1=(0, 0), point_2=(0, 0)):
+def distance(point_1=(0, 0), point_2=(0, 0)) -> float:
     """ Returns the distance between two points """
     return math.sqrt((point_1[0] - point_2[0]) ** 2 + (point_1[1] - point_2[1]) ** 2)
