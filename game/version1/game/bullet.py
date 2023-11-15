8a1f173a0ce83c28283341913874058aaeda5ddb
Troy Martin <beef.erikson.studios@gmail.com>
2023-11-14 22:25:52 -0500

    linting fix.

--- game/version1/game/bullet.py
+++ game/version1/game/bullet.py
@@ -13,6 +13,6 @@ class Bullet(physical_object.PhysicalObject):
         super().__init__(resources.bullet_image, *args, *kwargs)
         pyglet.clock.schedule_once(self.die, 0.5)  # Kills bullet after 0.5 seconds
 
-    def die(self, dt):
+    def die(self):
         """ Removes the bullet """
         self.dead = True
