5956c9c316d565e3298bbe0caeab54539ddbe266
Troy Martin <beef.erikson.studios@gmail.com>
2023-11-14 21:37:35 -0500

    Bullet creation and death.

---
+++ game/version1/game/bullet.py
@@ -0,0 +1,18 @@
+"""
+     Handles bullet logic.
+"""
+import pyglet
+from . import physical_object, resources
+
+
+class Bullet(physical_object.PhysicalObject):
+    """ Bullets fired by player. """
+
+    def __init__(self, *args, **kwargs):
+        """ Initializations. """
+        super().__init__(resources.bullet_image, *args, *kwargs)
+        pyglet.clock.schedule_once(self.die, 0.5)  # Kills bullet after 0.5 seconds
+
+    def die(self, dt):
+        """ Removes the bullet """
+        self.dead = True
